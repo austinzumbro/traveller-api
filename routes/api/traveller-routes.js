b82e0ab1067ce6cd93a7fefd0466d4b62e4fdc83
Austin Zumbro <azumbro@gmail.com>
2023-05-03 11:28:54 -0700

    traveller routes written

--- routes/api/traveller-routes.js
+++ routes/api/traveller-routes.js
@@ -24,3 +24,12 @@ router.get('/:id', async (req, res) => {
         res.status(500).json(err);
     }
 })
+
+router.post('/', async (req, res) => {
+    try {
+        const tavellerData = await Traveller.create(req.body);
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
